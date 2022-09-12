7f9e38de639fcb725e0603dd056446b9bdd32abe
URealTech <abdulaziz.sudo@gmail.com>
2022-09-12 16:59:00 +0300

    Commit

--- QRCodeGeneratorAndEmployeeManager/Models/Key.cs
+++ QRCodeGeneratorAndEmployeeManager/Models/Key.cs
@@ -3,6 +3,6 @@
     public class Key
     {
         public string symetricKey = "ePPhSSJIguIlaLys";
-        public string SymetricKey { get; }    
+        public string SymetricKey { get { return symetricKey; } }    
     }
 }
